8b5db402d3249936e729a5c46616470a4be90eef
shikharbahikmagar <shikharbahik5@gmail.com>
2025-06-01 22:04:09 +0545

    few changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -40,7 +40,7 @@ const Project = () => {
               <button
                 onClick={() => setActiveTab(tab.label)}
                 className={`
-                  relative px-5 py-1.5 rounded-full text-sm font-medium
+                  relative px-5 py-1.5 rounded-full font-medium
                   transition-all duration-300 ease-in-out text-center text-sm
                   ${activeTab === tab.label 
                     ? 'bg-[#0A1627] text-[#3B82F6] ring-1 ring-[#1D4ED8]/30  before:absolute before:inset-0 before:rounded-full before:ring-1 before:ring-[#3B82F6]/20 before:shadow-[0_0_8px_rgba(59,130,246,0.3)] before:-z-10' 
@@ -65,13 +65,12 @@ const Project = () => {
               <img 
                 src={cephas} 
                 alt="Flyer design"
-                className="w-full h-full relative object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+                className="w-full h-full relative object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
               <div className="absolute inset-0  bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold text-white mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">CEPHAS</h3>
-                <p className="text-sm text-gray-400 text-gray-300 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam volutpat
+                <h3 className="text-2xl font-semibold mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">CEPHAS</h3>
+                <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
                 </p>
               </div>
             </div>
@@ -79,24 +78,24 @@ const Project = () => {
          </a>
 
           {/* Banner Design Card */}
-         <a href="https://publicbuilders.com.np/" target="_blank">
-         <div className="group relative rounded-2xl overflow-hidden">
+          <a target='_blank' href="https://gssparkle.com/">
+          <div className="group relative rounded-2xl overflow-hidden">
             <div className="aspect-square relative">
-              <img 
-                src={publicbuilders}
-                alt="Banner design"
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+             <img 
+                src="https://gssparkle.com/storage/images/hero/1740641690.jpg" 
+                alt="Pattern design"
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
+            
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold text-white mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">Public Builders</h3>
-                <p className="text-sm text-gray-400 text-gray-300 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet.
+                <h3 className="text-2xl font-semibold opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out mb-2 text-gray-400 font-sans">GsSparkle</h3>
+                <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
                 </p>
               </div>
             </div>
           </div>
-         </a>
+          </a>
  
           {/* Disk Cover Card */}
          <a href="https://gyhealthcare.my/" target='_blank'>
@@ -104,13 +103,12 @@ const Project = () => {
             <div className="aspect-square relative">
               <img 
                 src={gyhealth}
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold text-white mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">GY Health-Care</h3>
-                <p className="text-sm text-gray-400 text-gray-300 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet.
+                <h3 className="text-2xl font-semibold mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">GY Health-Care</h3>
+                <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
                 </p>
               </div>
             </div>
@@ -124,14 +122,13 @@ const Project = () => {
              <img 
                 src="https://s3-alpha.figma.com/hub/file/1803556575/dddad4ab-afbe-4c4b-bfbd-5f872db64b12-cover.png" 
                 alt="Pattern design"
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
             
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-white mb-2 text-gray-400 font-sans">Youtube Clone</h3>
+                <h3 className="text-2xl font-semibold opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out mb-2 text-gray-400 font-sans">Youtube Clone</h3>
                 <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam volutpat vitae risus eget maximus. Pellentesque euismod.
                 </p>
               </div>
             </div>
@@ -143,13 +140,12 @@ const Project = () => {
               <img 
                 src="https://edusuitepk.b-cdn.net/wp-content/uploads/2021/12/Fee-Management-.jpg" 
                 alt="Banner design"
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold text-white mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">Library Management System</h3>
-                <p className="text-sm text-gray-400 text-gray-300 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam volutpat vitae risus eget maximus. Pellentesque euismod.
+                <h3 className="text-2xl font-semibold mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">Library Management System</h3>
+                <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
                 </p>
               </div>
             </div>
@@ -161,13 +157,12 @@ const Project = () => {
               <img 
                 src="https://asset.gecdesigns.com/img/isometric-illustration/the-future-of-retail-e-commerce-concept-vector-illustration-1682432685910-cover.webp" 
                 alt="Disk cover"
-                className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 transition-all duration-1000 ease-in-out"
+                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-1000 ease-in-out"
               />
               <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,1)]"></div>
               <div className="absolute bottom-0 left-0 p-6">
-                <h3 className="text-2xl font-semibold text-white mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">E-commerce</h3>
-                <p className="text-sm text-gray-400 text-gray-300 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam volutpat vitae risus eget maximus. Pellentesque euismod.
+                <h3 className="text-2xl font-semibold mb-2 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out text-gray-400 font-sans">E-commerce</h3>
+                <p className="text-sm text-gray-400 opacity-50 group-hover:opacity-100 transition-all duration-1000 ease-in-out font-sans">
                 </p>
               </div>
             </div>
